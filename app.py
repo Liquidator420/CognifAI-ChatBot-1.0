75cb760b2e6e25b19468b14879e6cfb38f59aa76
Sachin Sushil Singh <100297153+Liquidator420@users.noreply.github.com>
2023-09-04 17:53:56 +0530

    Update app.py
    updated app.py for new frontend

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_get():
-    return render_template("base.html")
+    return render_template("your_chat_interface.html")
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    text = request.get_json().get("message")
-    response = get_response(text) 
-    message = {"answer": response}
-    return jsonify(message)
+@app.route("/get", methods=["POST"])
+def get():
+    user_message = request.form.get("msg")
+    bot_response = get_response(user_message)  # Use your chatbot function here
+    return jsonify(bot_response)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
  
